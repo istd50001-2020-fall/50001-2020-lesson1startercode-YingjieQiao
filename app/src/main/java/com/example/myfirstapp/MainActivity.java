52cbd3524e17f96ace71e027d465a69b6e03bce2
yingjie <yingjie_qiao@outlook.com>
2020-11-05 17:06:12 +0800

    lab1 done

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -8,21 +8,24 @@ package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
+import java.util.Random;
 
-
-//TODO 1.1 Put in some images in the drawables folder
-//TODO 1.2 Go to activity_main.xml and modify the layout
 
 public class MainActivity extends AppCompatActivity {
 
-    //TODO 1.2 Instance variables are declared for you, please import the libraries
+    private static final String LOG_TAG =
+            MainActivity.class.getSimpleName();
     ArrayList<Integer> images;
     Button charaButton;
+    Button firstImageButton;
     ImageView charaImage;
     int count = 0;
 
@@ -31,14 +34,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //TODO 1.3 Instantiate An ArrayList object
-        //TODO 1.4 Add the image IDs to the ArrayList
-        //TODO 1.5 Get references to the charaButton and charaImage widgets using findViewById
-        //TODO 1.6 For charaButton, invoke the setOnClickListener method
-        //TODO 1.7 Create an anonymous class which implements View.OnClickListener interface
-        //TODO 1.8 Within onClick, write code to randomly select an image ID from the ArrayList and display it in the ImageView
-        //TODO 1.9 [On your own] Create another button, which when clicked, will cause one image to always be displayed
+        images = new ArrayList<>();
+        images.add(R.drawable.ashketchum);
+        images.add(R.drawable.bartsimpson);
+        images.add(R.drawable.edogawaconan);
+        images.add(R.drawable.judyhopps);
+        images.add(R.drawable.nemo);
+        images.add(R.drawable.nickwilde);
+        images.add(R.drawable.pikachu);
+        images.add(R.drawable.snorlax);
+        images.add(R.drawable.tomandjerry);
+        images.add(R.drawable.yoda);
 
+        charaButton = findViewById(R.id.charaButton);
+        firstImageButton = findViewById(R.id.firstImageButton);
+        charaImage = findViewById(R.id.charaImage);
+
+        charaButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Log.d(LOG_TAG, "Image Button clicked!");
+                Random random = new Random();
+                int index = random.nextInt(images.size()-1);
+                int id = images.get(index);
+                //System.out.println(id);
+                charaImage.setImageResource(id);
+            }
+        });
+
+        firstImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(LOG_TAG, "First Image Button clicked!");
+                int id = images.get(0);
+                //System.out.println(id);
+                charaImage.setImageResource(id);
+            }
+        });
 
     }
 }
